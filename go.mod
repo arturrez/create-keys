3a95351eff10896ea59be62e408f2dda5a0020db
Artur Reznikov <artur.reznikov@avalabs.org>
2023-01-17 22:24:42 -0800

    feat(sync): wip

--- go.mod
+++ go.mod
@@ -2,9 +2,25 @@ module github.com/arturrez/create-keys
 
 go 1.19
 
-require github.com/ava-labs/avalanchego v1.9.7
+require (
+	github.com/ava-labs/avalanchego v1.9.7
+	github.com/aws/aws-sdk-go v1.44.181
+	github.com/aws/aws-sdk-go-v2/config v1.18.8
+	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.18.1
+)
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.17.3 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.27 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.28 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.21 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.18.0 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -2,34 +2,79 @@ package sync
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager/types"
 	"github.com/aws/aws-sdk-go/aws"
 )
 
-func syncSecret(
-	ctx context.Context,
-	secretName string,
-	region string,
-	secretData map[string]string) error {
+const secretTag string = "avalanchego-secret-operator"
 
+type api interface {
+	CreateSecret(ctx context.Context, params *secretsmanager.CreateSecretInput, optFns ...func(*secretsmanager.Options)) (*secretsmanager.CreateSecretOutput, error)
+	DescribeSecret(ctx context.Context, params *secretsmanager.DescribeSecretInput, optFns ...func(*secretsmanager.Options)) (*secretsmanager.DescribeSecretOutput, error)
+}
+
+type SecretsManager struct {
+	client api
+	region string
+	ctx    context.Context
+}
+
+// New returns a SecretsManager configured with the default session.
+func New(ctx context.Context,
+	region string,
+) (*SecretsManager, error) {
 	config, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	svc := secretsmanager.NewFromConfig(config)
+	return &SecretsManager{
+		client: svc,
+		region: region,
+		ctx:    ctx,
+	}, nil
+}
 
-	// check if secret exists
-	input := &secretsmanager.GetSecretValueInput{
-		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+// todo add KMS support
+func (s *SecretsManager) createSecret(
+	secretID string,
+	secretData map[string]string) (string, error) {
+
+	secretJson, err := json.Marshal(secretData)
+	if err != nil {
+		return "", err
+	}
+	secretDescription := "Managed by " + secretTag
+	input := &secretsmanager.CreateSecretInput{
+		Name:                        aws.String(secretID),
+		ForceOverwriteReplicaSecret: false,
+		Description:                 &secretDescription,
+		SecretBinary:                secretJson,
+		Tags:                        s.generateTags(),
 	}
 	// check if secret exists
-	result, err := svc.GetSecretValue(ctx, input)
+	_, err = s.client.DescribeSecret(s.ctx, &secretsmanager.DescribeSecretInput{SecretId: aws.String(secretID)})
 	if err == nil {
-		return fmt.Errorf("Secret exists. Aborting secret sync")
+		return "", fmt.Errorf("secret exists. Aborting secret sync")
 	}
-	return nil
+
+	//write secretData
+	out, err := s.client.CreateSecret(s.ctx, input)
+	if err != nil {
+		return "", err
+	}
+
+	return *out.ARN, nil
+}
+
+func (s *SecretsManager) generateTags() []types.Tag {
+	tags := []types.Tag{}
+	timestamp := time.Now().UTC().Format(time.UnixDate)
+	return append(tags, types.Tag{Key: aws.String(secretTag), Value: aws.String(timestamp)})
 }
