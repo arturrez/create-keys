0394e495cce08aad3a4349fabbec341240676451
Artur Reznikov <artur.reznikov@avalabs.org>
2023-01-18 10:25:36 -0800

    fix(gen); wip

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -1,15 +1,11 @@
 package gen
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/base64"
-	"fmt"
 
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/staking"
 	"github.com/ava-labs/avalanchego/utils/crypto/bls"
-	"github.com/ava-labs/avalanchego/utils/hashing"
 )
 
 const stakerKey = "staker.key_BASE64"
@@ -39,7 +35,7 @@ func GenerateKeys() (map[string]string, error) {
 	blsPublicBytes := bls.PublicKeyToBytes(bls.PublicFromSecretKey(blsSignerKey))
 
 	return map[string]string{
-		stakerCert: base64.StdEncoding.EncodeToString(kBytes),
+		stakerCert: base64.StdEncoding.EncodeToString(cBytes),
 		stakerKey:  base64.StdEncoding.EncodeToString(kBytes),
 		signerKey:  base64.StdEncoding.EncodeToString(blsSignerBytes),
 		publicKey:  string(blsPublicBytes),
@@ -48,17 +44,11 @@ func GenerateKeys() (map[string]string, error) {
 }
 
 func getNodeID(certBytes []byte, keyBytes []byte) (string, error) {
-	cert, err := tls.X509KeyPair(certBytes, keyBytes)
+	cert, err := staking.LoadTLSCertFromBytes(keyBytes, certBytes)
+
 	if err != nil {
 		return "", err
 	}
-	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
-	if err != nil {
-		return "", err
-	}
-	nodeID, err := ids.ToShortID(hashing.PubkeyBytesToAddress(cert.Leaf.Raw))
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s", nodeID), nil
+
+	return ids.NodeIDFromCert(cert).String(), nil
 }
--- pkg/gen/gen_test.go
+++ pkg/gen/gen_test.go
@@ -1,15 +1,21 @@
 package gen
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestGenerateKeys(t *testing.T) {
 	r, err := GenerateKeys()
+	fmt.Println(r)
 	if err != nil {
 		t.Error("GenerateKeys returns error")
 	}
 	if _, ok := r[stakerCert]; !ok {
 		t.Error(stakerCert + " not found in GenerateKeys return")
+	}
+	if !strings.HasPrefix("NodeID", r[nodeID]) {
+		t.Error(nodeID + " not found in GenerateKeys return")
 	}
 }
